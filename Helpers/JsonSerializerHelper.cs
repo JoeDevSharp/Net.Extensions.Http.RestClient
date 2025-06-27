923ed2bf77d5221f0edca5f8e65d701d822e845a
José Miguel <j.ramos@jm-ramos.com>
2025-06-27 12:22:24 +0200

    Add JsonSerializerHelper utility class
    Introduces a new Helpers project targeting .NET 8.0 and adds a static JsonSerializerHelper class for simplified JSON serialization and deserialization with default options.

---
+++ Helpers/JsonSerializerHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace Helpers
+{
+    public static class JsonSerializerHelper
+    {
+        private static readonly JsonSerializerOptions DefaultOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false
+        };
+
+        public static string Serialize<T>(T obj, JsonSerializerOptions? options = null)
+        {
+            return JsonSerializer.Serialize(obj, options ?? DefaultOptions);
+        }
+
+        public static T? Deserialize<T>(string json, JsonSerializerOptions? options = null)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<T>(json, options ?? DefaultOptions);
+            }
+            catch (JsonException)
+            {
+                // Aquí puedes loggear o manejar error si quieres
+                return default;
+            }
+        }
+    }
+}
